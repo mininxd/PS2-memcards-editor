3b6c49f00c97a2ceac9be9136ba1be202b8454a3
mininxd <mininsna@gmail.com>
2026-09-10 10:47:26 +0000

    Set app version to 1.1.1

--- app/src/main/java/xyz/mininxd/ps2memcards/ui/MainActivity.kt
+++ app/src/main/java/xyz/mininxd/ps2memcards/ui/MainActivity.kt
@@ -408,9 +408,9 @@ class MainActivity : ComponentActivity() {
                                     updateStatus = updateStatus,
                                     onCheckUpdate = {
                                         val version = try {
-                                            packageManager.getPackageInfo(packageName, 0).versionName ?: "1.1.2"
+                                            packageManager.getPackageInfo(packageName, 0).versionName ?: "1.1.1"
                                         } catch (e: Exception) {
-                                            "1.1.2"
+                                            "1.1.1"
                                         }
                                         viewModel.checkUpdate(version)
                                     }
